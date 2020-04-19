0431ad81f9c36e4da3b56c071af6cd71a1cc8485
Clement Lemaire <lemairec02@gmail.com>
2020-04-19 15:28:49 +0200

    install_agrigpspi

---
+++ install_agrigpspi/install.sh
@@ -0,0 +1,4 @@
+cp ~/agripi/install_agrigpspi/agripi.desktop ~/Desktop
+sudo chmod +x ~/Desktop/agripi.desktop
+mkdir -p ~/.config/autostart
+cp ~/agripi/install_agrigpspi/agripi_autostart.desktop ~/.config/autostart
