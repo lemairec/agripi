a262db1874ed18c4ba75adb05533ace635fe05e6
Clement Lemaire <lemairec02@gmail.com>
2021-04-09 14:34:48 +0200

    My serial

--- my_serial.py
+++ my_serial.py
@@ -1,12 +1,34 @@
 import serial
 from tkinter import *
+import serial.tools.list_ports
 
 
-root = Tk()
-T = Text(root, height=20, width=80)
+import tkinter as tk
+from tkinter import ttk
+ 
+app = tk.Tk() 
+app.geometry('800x600')
+
+labelTop = tk.Label(app,
+                    text = "Serials")
+labelTop.grid(column=0, row=0)
+
+my_serials = []
+ports = serial.tools.list_ports.comports()
+for port, desc, hwid in sorted(ports):
+    my_serials.append(port)
+
+comboExample = ttk.Combobox(app, 
+                            values=my_serials)
+comboExample.grid(column=0, row=1)
+comboExample.current(1)
+
+print(comboExample.get())
+
+T = Text(app, height=20, width=80)
 T.pack()
-T.insert(END, "Just a text Widget\nin two lines\n")
-
+T.insert(END, "Output")
+T.grid(column=0, row=2)
 
 ser = serial.Serial(
     port='/dev/cu.usbmodem1412201',
@@ -15,13 +37,18 @@ ser = serial.Serial(
 
 def readSerial():
     response = ser.readline()
-    print(response)
     T.insert(END, response)
     T.see(END)
-    root.after(50, readSerial) # after 50 milli seconds this is called again
+    app.after(50, readSerial) # after 50 milli seconds this is called again
 
 readSerial()
 
-root.mainloop()
+T = Text(app, height=20, width=5        )
+T.pack()
+T.insert(END, "Output")
+T.grid(column=0, row=2)
+
+app.mainloop()
+
 
 
